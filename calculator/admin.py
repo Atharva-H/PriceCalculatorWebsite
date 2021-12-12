9b184ca58753a2fd00713a8b6985d589cbe8e7f4
JigarJoshi <jigar.pj@somaiya.edu>
2021-12-12 22:41:52 +0530

    add: models skeleton added

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import TestData
 
-admin.site.register(TestData)
+@admin.register(TestData)
+class TestDataAdmin(admin.ModelAdmin):
+    list_display = (
+        'text1','text2', 'num1'
+    )
+    search_fields = (
+        'text1','text2', 'num1'
+    )
--- calculator/models.py
+++ calculator/models.py
@@ -1,4 +1,64 @@
 from django.db import models
+from uuid import uuid4
+from datetime import datetime
+
+# class Buyer(models.Model):
+#     buyer_id = models.CharField(max_length=100, blank=True, unique=True, default=uuid4)
+#     buyer_alias = models.CharField(max_length=20, blank= False, null=False)
+#     company_name = models.CharField(max_length=20, blank= False, null=False)
+#     company_address = models.TextField(max_length=1000, blank= False, null=False)
+#     primary_phone_number =  models.CharField(max_length=10, blank= False, null=False)
+#     secondary_phone_number = models.CharField(max_length=10, blank= False, null=False)
+#     email_id =  models.CharField(max_length=100, blank= False, null=False)
+#     terms_and_conditions = models.TextField(max_length=500, blank= False, null= False)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(default=datetime.now())
+
+# class MasterCarton(models.Model):
+#     carton_id = models.CharField(max_length=100, blank=True, unique=True, default=uuid4)
+#     length = models.IntegerField()
+#     breadth = models.IntegerField()
+#     height = models.IntegerField()
+
+# class Item(models.Model):
+    
+    
+    
+#     gsm = models.IntegerField()
+#     paper_rate = models.FloatField()
+#     scrape_rate = models.FloatField()
+#     margin = models.FloatField()
+#     cost_per_cup = models.FloatField()
+#     cups_per_sku = models.IntegerField()
+#     sku_per_mc = models.IntegerField()
+#     print_cost = models.FloatField()
+#     mc_cost = models.FloatField()
+#     freight_per_container = models.FloatField()
+#     mc_per_container = models.IntegerField()
+#     dollor_rate = models.FloatField()
+#     add_cost_per_sku = models.FloatField()
+
+
+#     size_of_cup = models.CharField(max_length=10)
+#     product_name = models.CharField(max_length=10)
+#     num_of_sku = models.IntegerField()
+#     rate_of_sku = models.IntegerField()
+#     product_desription = models.TextField(max_length=20000)
+#     num_of_mc = models.IntegerField()
+#     sku_per_mc = models.IntegerField()
+
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(default=datetime.now())
+
+#     master_carton_ref = models.ForeignKey()
+#     buyer_ref = models.ForeignKey()
+#     quotation_id = models.CharField(max_length=20)
+
+# # class Quotation():
+    
+    
+
+
 
 
 class TestData(models.Model):
