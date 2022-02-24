9f1d4a6205b36e163ab3ceb6d51d71eeb5472e22
Atharva-H <atharva.humar@somaiya.edu>
2022-02-24 14:24:53 +0530

    commeted: STATICFILES_DIRS

--- calculator/settings.py
+++ calculator/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+DISABLE_COLLECTSTATIC = 1
 # STATICFILES_DIRS = ["calculator\static"]
 
 # Default primary key field type
